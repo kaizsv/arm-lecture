f3f25e166fd070ad80e6fbcb3aba10a9d5069dbc
kaizsv <kai.zsv@gmail.com>
2015-09-27 17:30:47 +0800

    Lab-1i Non-Recursion Fibonacci
    Edit version 4 of "fib.s" for homework 1

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,24 +11,25 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r2, r3, r4, r5, lr}
+	push {r3, r4, r5, r6, lr}
 	
 	mov r3, #0          	 @R3 = 0 (fib(0))
 	mov r4, #1	    	 @R4 = 1 (fib(1))
-	adds r2, r0, #4294967295 @R2 = R0 - 1
-	bgt .Loop	    	 @If (R2 > 1) goto .Loop
+	adds r6, r0, #4294967295 @R6 = R0 - 1
+	bgt .Loop	    	 @If (R6 > 1) goto .Loop
 			    	 @else return R0
-	pop {r2, r3, r4, r5, pc}
+
+	pop {r3, r4, r5, r6, pc}
 
 .Loop:
 	add r5, r3, r4      	 @R5 = R3 + R4
 	mov r3, r4	    	 @R3 = R4
 	mov r4, r5	    	 @R4 = R5
-	adds r2, r2, #4294967295 @R2 = R2 - 1
-	bgt .Loop	    	 @If (R2 >= 0) goto .Loop
+	adds r6, r6, #4294967295 @R6 = R6 - 1
+	bgt .Loop	    	 @If (R6 >= 0) goto .Loop
 	mov r0, r5		 @else return R5
 
-	pop {r2, r3, r4, r5, pc}
+	pop {r3, r4, r5, r6, pc}
 
 	.size fibonacci, .-fibonacci
 	.end
