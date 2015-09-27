beff18d8593d27bad90f49c25c67738d25df8676
kaizsv <kai.zsv@gmail.com>
2015-09-27 19:15:29 +0800

    Lab-1 Recursive Fibonacci
    Version 2 of "fib.s"

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,25 +11,25 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, r6, lr}
+	push {r4, r5, r6, r7, lr}
 	
-	mov r3, #0          	 @R3 = 0 (fib(0))
-	mov r4, #1	    	 @R4 = 1 (fib(1))
-	adds r6, r0, #4294967295 @R6 = R0 - 1
-	bgt .Loop	    	 @If (R6 > 1) goto .Loop
+	mov r5, #0          	 @R5 = 0 (fib(0))
+	mov r6, #1	    	 @R6 = 1 (fib(1))
+	adds r4, r0, #4294967295 @R4 = R0 - 1
+	bgt .Loop	    	 @If (R4 > 1) goto .Loop
 			    	 @else return R0
 
-	pop {r3, r4, r5, r6, pc}
+	pop {r4, r5, r6, r7, pc}
 
 .Loop:
-	add r5, r3, r4      	 @R5 = R3 + R4
-	mov r3, r4	    	 @R3 = R4
-	mov r4, r5	    	 @R4 = R5
-	adds r6, r6, #4294967295 @R6 = R6 - 1
-	bgt .Loop	    	 @If (R6 >= 0) goto .Loop
-	mov r0, r5		 @else return R5
+	add r7, r5, r6      	 @R7 = R5 + R6
+	mov r5, r6	    	 @R5 = R6
+	mov r6, r7	    	 @R6 = R7
+	adds r4, r4, #4294967295 @R4 = R4 - 1
+	bgt .Loop	    	 @If (R4 >= 0) goto .Loop
+	mov r0, r7		 @else return R7
 
-	pop {r3, r4, r5, r6, pc}
+	pop {r4, r5, r6, r7, pc}
 
 	.size fibonacci, .-fibonacci
 	.end
